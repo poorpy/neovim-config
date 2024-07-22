b359b9c1b648f40162e09aa247ca3cc2b863a822
Bartosz Marczyński <marczynski.bartosz@gmail.com>
2024-07-22 10:53:47 +0200

    fix: update which-key bindings

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,14 +2,21 @@ return {
     "folke/which-key.nvim",
     config = function()
         require("which-key").setup()
-        require("which-key").register {
-            ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-            ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-            ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-            ["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
-            ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-            ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-            ["<leader>f"] = { name = "[F]lash", _ = "which_key_ignore" },
+        require("which-key").add {
+            { "<leader>c", group = "[C]ode" },
+            { "<leader>c_", hidden = true },
+            { "<leader>d", group = "[D]ocument" },
+            { "<leader>d_", hidden = true },
+            { "<leader>f", group = "[F]lash" },
+            { "<leader>f_", hidden = true },
+            { "<leader>g", group = "[G]it" },
+            { "<leader>g_", hidden = true },
+            { "<leader>r", group = "[R]ename" },
+            { "<leader>r_", hidden = true },
+            { "<leader>s", group = "[S]earch" },
+            { "<leader>s_", hidden = true },
+            { "<leader>w", group = "[W]orkspace" },
+            { "<leader>w_", hidden = true },
         }
     end,
 }
