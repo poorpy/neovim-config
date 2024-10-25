c71ce3c8addca9f7fefa191c4f1cd95e579420c2
Bartosz Marczyński <marczynski.bartosz@gmail.com>
2024-10-25 22:40:58 +0200

    feat: add golines to conform config

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -133,7 +133,7 @@ M.setup = function()
     require("conform").setup {
         formatters_by_ft = {
             lua = { "stylua" },
-            go = { "goimports" },
+            go = { "goimports", "golines" },
             zig = { "zigfmt" },
         },
     }
